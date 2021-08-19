bb159fbbcdb850e2ea2b5e3ca1c55f36c8474c43
Francesco Lupano <lupano.web90@gmail.com>
2021-08-19 09:42:13 +0200

    Initialize repository

---
+++ lib/object_palindrome.rb
@@ -0,0 +1,6 @@
+require "object_palindrome/version"
+
+module ObjectPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/object_palindrome/version.rb
@@ -0,0 +1,3 @@
+module ObjectPalindrome
+  VERSION = "0.1.0"
+end
