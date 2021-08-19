3d3f4442e26fdd4c619321654b89d15926567176
Francesco Lupano <lupano.web90@gmail.com>
2021-08-19 14:38:55 +0200

    refactoring

--- test/object_palindrome_test.rb
+++ test/object_palindrome_test.rb
@@ -1,5 +1,19 @@
 require "test_helper"
 
 class ObjectPalindromeTest < Minitest::Test
+  def test_not_palindrome
+    refute "Apple".palindrome?
+  end
 
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_punctuation
+    assert "Madam, I'm Adam".palindrome?
+  end
 end
